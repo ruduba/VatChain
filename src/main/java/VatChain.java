16c2396b33c0c731bafdbbdafe305f5df6c1a442
ruduba <vatbudur@gmail.com>
2025-12-02 19:33:18 +0530

    added hash verification

--- src/main/java/VatChain.java
+++ src/main/java/VatChain.java
@@ -3,7 +3,6 @@ import com.google.gson.GsonBuilder;
 
 public class VatChain {
 	public static ArrayList<Block> blockchain = new ArrayList<Block>();
-	
 	public static void main(String[]args) {
 
 		blockchain.add(new Block("Hi i am the first block", "0"));
@@ -14,4 +13,24 @@ public class VatChain {
 		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
 		System.out.println(blockchainJson);
 	}
+	public static Boolean isChainValid() {
+		Block currentBlock;
+		Block previousBlock;
+		
+		for(int i =1; i<blockchain.size();i++) {
+			currentBlock = blockchain.get(i);
+			previousBlock = blockchain.get(i-1);
+			
+			if(!currentBlock.hash.equals(currentBlock.calculateHash())) {
+				System.out.println("Current Hashes NOT EQUAL");
+				return false;
+			}
+			if(!previousBlock.hash.equals(previousBlock.calculateHash())) {
+				System.out.println("Previous Hashes NOT EQUAL");
+				return false;
+			}
+		}
+		return true;
+	}
+	
 }
