fb34ddac217d29be08f835b97dd932dbe0855077
ruduba <vatbudur@gmail.com>
2025-12-02 20:08:20 +0530

    added proof of work + mining

--- src/main/java/VatChain.java
+++ src/main/java/VatChain.java
@@ -3,12 +3,20 @@ import com.google.gson.GsonBuilder;
 
 public class VatChain {
 	public static ArrayList<Block> blockchain = new ArrayList<Block>();
+	public static int difficulty = 5;
 	public static void main(String[]args) {
 
 		blockchain.add(new Block("Hi i am the first block", "0"));
+		System.out.println("Trying to Mine block 1...");
+		blockchain.get(0).mineBlock(difficulty);
 		blockchain.add(new Block("Hi i am the second block", blockchain.get(blockchain.size()-1).hash));
+		System.out.println("Trying to Mine block 2...");
+		blockchain.get(1).mineBlock(difficulty);
 		blockchain.add(new Block("Hi i am the thiiird block", blockchain.get(blockchain.size()-1).hash));
-
+		System.out.println("Trying to Mine block 3...");
+		blockchain.get(2).mineBlock(difficulty);
+		
+		System.out.println("\nBlockChain is Valid: "+ isChainValid());
 		
 		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
 		System.out.println(blockchainJson);
