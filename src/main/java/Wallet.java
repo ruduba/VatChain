41413c061b3b84b630569b9592146705d3d53e63
ruduba <vatbudur@gmail.com>
2025-12-03 16:16:01 +0530

    added balance checking and fund sending functionality

--- src/main/java/Wallet.java
+++ src/main/java/Wallet.java
@@ -1,9 +1,14 @@
 import java.security.*;
 import java.security.spec.ECGenParameterSpec;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class Wallet {
 	public PrivateKey privateKey;
 	public PublicKey publicKey;
+	
+	public HashMap<String, TransactionOutput> UTXOs = new HashMap<String, TransactionOutput>();
 	
 	public Wallet() {
 		generateKeyPair();
@@ -24,4 +29,43 @@ public class Wallet {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	public float getBalance() {
+		float total = 0;
+		for(Map.Entry<String, TransactionOutput> item: VatChain.UTXOs.entrySet()) {
+			TransactionOutput UTXO = item.getValue();
+			if(UTXO.isMine(publicKey)) {
+				UTXOs.put(UTXO.id, UTXO);
+				total += UTXO.value;
+			}
+			
+		}
+		return total;
+	}
+	
+	public Transaction sendFunds(PublicKey _reciepient, float value) {
+		if(getBalance()<value) {
+			System.out.println("#Not enough funds to send transaction. Transaction Discarded");
+			return null;
+		}
+		
+		ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+		
+		float total =0;
+		for(Map.Entry<String, TransactionOutput> item: UTXOs.entrySet()) {
+			TransactionOutput UTXO = item.getValue();
+			total += UTXO.value;
+			inputs.add(new TransactionInput(UTXO.id));
+			if(total > value) break;
+		}
+		
+		Transaction newTransaction = new Transaction(publicKey, _reciepient, value, inputs);
+		newTransaction.generateSignature(privateKey);
+		
+		for(TransactionInput input : inputs) {
+			UTXOs.remove(input.txOutputId);
+		}
+		return newTransaction;
+	}
+	
 }
